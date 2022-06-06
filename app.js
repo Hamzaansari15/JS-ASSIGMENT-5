1dfba79f5f90d2deac133903de542669a7ea7fea
Hamzaansari <hamzaansaree15@gmail.com>
2022-06-06 01:54:54 -0700

    Update JS file

---
+++ app.js
@@ -0,0 +1,55 @@
+// var multiArray = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+// for(var i = 0; i < multiArray.length; i++){
+//     console.log(multiArray[i]);
+//     document.write("<h1>" + multiArray[i] + "</h1>");
+// // }
+// var arr = [1,2,3,4,5,6,7,8,9,10];
+// for(var i = 0; i < arr[i]; i++){
+//     document.write("<h1>" + arr[i] + "</h1>");
+// }    
+// var tableNumbar = +prompt("Enter the number to show its table");
+// var tableLength = +prompt("Enter the length of table");
+// var count = 0;
+// for(var i = 0; i < tableLength; i++){
+//     document.write("<h1>" + tableNumbar + " X " + ++count + " = " + tableNumbar * count + "</h1>");
+// // }
+// var array = ["apple", "banana", "mango", "orange","strawberry"];
+// for(var i = 0; i < array.length; ++i){
+//     document.write("<h1>" + array[i] + "</h1>");
+// }
+// var arr = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"];
+// for(var i = 0; i < arr.length; i++){
+//     document.write("<b>" + arr[i] + "</b>");
+// // }
+// var arr = [10,9,8,7,6,5,4,3,2,1];
+// for(var i = 0; i < arr.length; i++){
+//     document.write("<b>" + arr[i] + "</b>");
+// }
+// var arr = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var order = prompt("welcome to ABC Bakery. What do you want to order sir/mam");
+// var flag = "Not available";
+// for(var i = 0; i < arr.length; i++){
+//     if(arr[i] === order){
+//         flag = "available";
+//         document.write("<h1>" + order + " is " + flag + " at index " + i + " in Bakery </h1>" );
+//     }
+    
+// }if(flag === "Not available"){
+//     document.write("<h1> We are sorry." + order + " is " + flag + " in our bakery </h1>");
+// }
+// var arr = [22,44,12,78,56,45,30,99,67,03,57,101,73,89];
+// var max = 0;
+// for(var i = 0; i < arr.length; i++){
+//     if(max < arr[i]){
+//         max = arr[i];
+//     }
+// }document.write("<h1> The array is 22,44,12,78,56,45,30,99,67,03,57,101,73,89 </h1>")
+// document.write("<h1> The greatesr value is " + max + "</h1>");
+// var arr = [22,33,67,15,30,28];
+// var min = arr[0];
+// for(var i = 0; i < arr.length; i++){
+//     if(min > arr[i]){
+//         min = arr[i];
+//     }
+// }document.write("<h1> The array is 22,33,67,15,30,28 </h1>");
+// document.write("<h1> The smallest value is " + min + "</h1>");
